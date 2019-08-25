c2b805503f3cb1e3aaae19fe0728cd297e55c5e3
NirabhraDas013 <nirabhra12709@gmail.com>
2019-08-25 12:16:03 +0530

    aa replica Files

---
+++ Assets/GameController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    private bool isEndGame = false;
+
+    public Rotator rotator;
+    public Spawner spawner;
+    public Animator animator;
+
+    public void EndGame()
+    {
+        if (isEndGame)
+            return;
+
+        isEndGame = true;
+
+        rotator.enabled = false;
+        spawner.enabled = false;
+
+        animator.SetTrigger("EndGame");
+    }
+
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
---
+++ Assets/Pin.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pin : MonoBehaviour
+{
+    private bool isPinned = false;
+
+    public float speed = 20f;
+    public Rigidbody2D rb;
+    public float speedModifier = 5f;
+
+    void Update()
+    {
+        if(!isPinned)
+            rb.MovePosition(rb.position + Vector2.up * speed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.tag == "Rotator")
+        {
+            transform.SetParent(col.transform);
+            //col.GetComponent<Rotator>().speed += speedModifier;         //rotation speeds up for every pin that hits
+            //col.GetComponent<Rotator>().speed *= -1;                    //rotation reverses for every pin that hits
+            Score.pinCount++;
+            isPinned = true;
+        }
+        else if(col.tag == "Pin")
+        {
+            FindObjectOfType<GameController>().EndGame();
+        }
+    }
+}
